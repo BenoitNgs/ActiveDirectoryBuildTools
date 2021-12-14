c95765c22ade8460ef4385b859e4018916685b9b
BenoitNgs <benoit.nugues@gmail.com>
2021-12-14 13:00:49 +0100

    Update Add-zDnsServerSecondaryServerTransferToSecureServers.ps1

--- DNS/Add-zDnsServerSecondaryServerTransferToSecureServers.ps1
+++ DNS/Add-zDnsServerSecondaryServerTransferToSecureServers.ps1
@@ -3,15 +3,30 @@ Import-Module DnsServer
 function Add-zDnsServerSecondaryServerTransferToSecureServers {
     param(
         [Parameter(Mandatory = $true)][string]$ZoneName,
-        [Parameter(Mandatory = $true)][string]$IPAddressSecondaryServer
+        [Parameter(Mandatory = $true)][array]$IPAddressSecondaryServer,
+        [Parameter(Mandatory = $false)][string]$DNSPrimaryServer="localhost"
     )
 
-    $DNSZone = Get-DnsServerZone -Name $ZoneName
-    $lstSecondaryDnsServer = $DNSZone.SecondaryServers.IPAddressToString + $IPAddressSecondaryServer
-    $lstSecondaryDnsServer = $lstSecondaryDnsServer | Select-Object -Unique
-    Set-DnsServerPrimaryZone -Name $DNSZone.ZoneName -Notify Notifyservers -notifyservers $lstSecondaryDnsServer -SecondaryServers $lstSecondaryDnsServer -SecureSecondaries TransferToSecureServers
+    $DNSZone = Get-DnsServerZone -ComputerName $DNSPrimaryServer -Name $ZoneName | Select-Object *
+    $lstSecondaryDnsServer = @()
+    
+    ForEach($IPAddress in $IPAddressSecondaryServer){
+        $objSecondaryDNSServer = New-Object System.object
+        $objSecondaryDNSServer | Add-Member -name 'IPAddressSecondaryServer' -MemberType NoteProperty -Value $IPAddress
+        $lstSecondaryDnsServer += $objSecondaryDNSServer.IPAddressSecondaryServer
+    }
 
-    return $(Get-DnsServerZone -Name $DNSZone.ZoneName | Select-Object ZoneName,SecondaryServers,NotifyServers)
+    ForEach($IPAddress in $DNSZone.SecondaryServers.IPAddressToString){
+        $objSecondaryDNSServer = New-Object System.object
+        $objSecondaryDNSServer | Add-Member -name 'IPAddressSecondaryServer' -MemberType NoteProperty -Value $IPAddress
+        $lstSecondaryDnsServer += $objSecondaryDNSServer.IPAddressSecondaryServer
+    }
+
+    $lstSecondaryDnsServer = $lstSecondaryDnsServer | Select-Object -Unique
+
+    Set-DnsServerPrimaryZone -ComputerName $DNSPrimaryServer -Name $DNSZone.ZoneName -Notify Notifyservers -notifyservers $lstSecondaryDnsServer -SecondaryServers $lstSecondaryDnsServer -SecureSecondaries TransferToSecureServers
+
+    return $(Get-DnsServerZone -ComputerName $DNSPrimaryServer -Name $DNSZone.ZoneName | Select-Object ZoneName,SecondaryServers,NotifyServers)
 }
 
 Add-zDnsServerSecondaryServerTransferToSecureServers -ZoneName "teddycorp.lan" -IPAddressSecondaryServer "192.168.1.20"
