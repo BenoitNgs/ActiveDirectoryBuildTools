be649205e8e03f4d0df270b4075856b4f49609ff
BenoitNgs <benoit.nugues@gmail.com>
2021-12-14 16:57:30 +0100

    Update Remove-zDnsServerSecondaryServerTransferToSecureServers.ps1

--- DNS/Remove-zDnsServerSecondaryServerTransferToSecureServers.ps1
+++ DNS/Remove-zDnsServerSecondaryServerTransferToSecureServers.ps1
@@ -3,20 +3,27 @@ Import-Module DnsServer
 function Remove-zDnsServerSecondaryServerTransferToSecureServers {
     param(
         [Parameter(Mandatory = $true)][string]$ZoneName,
-        [Parameter(Mandatory = $true)][string]$IPAddressSecondaryServer
+        [Parameter(Mandatory = $true)][string]$IPAddressSecondaryServer,
+        [Parameter(Mandatory = $false)][string]$DNSPrimaryServer="localhost"
     )
 
-    $DNSZone = Get-DnsServerZone -Name $ZoneName
-    $lstDNSZoneSecondaryDnsServer = $DNSZone.SecondaryServers.IPAddressToString | Select-Object -Unique
+    $DNSZone = Get-DnsServerZone -ComputerName $DNSPrimaryServer -Name $ZoneName
+    $lstDNSZoneSecondaryDnsServer = $DNSZone.SecondaryServers.IPAddressToString
     $lstSecondaryDnsServer = @()
 
-    foreach($DNSZoneSecondaryDnsServer in $lstDNSZoneSecondaryDnsServer){
-        if($DNSZoneSecondaryDnsServer -ne $IPAddressSecondaryServer){$lstSecondaryDnsServer += $DNSZoneSecondaryDnsServer}
-    }
+    if($IPAddressSecondaryServer -eq "*"){
+        foreach($DNSZoneSecondaryDnsServer in $lstDNSZoneSecondaryDnsServer){
+            Set-DnsServerPrimaryZone -ComputerName $DNSPrimaryServer -Name $DNSZone.ZoneName -Notify NoNotify -SecureSecondaries NoTransfer
+        }
+    }else{
+        foreach($DNSZoneSecondaryDnsServer in $lstDNSZoneSecondaryDnsServer){
+            if($DNSZoneSecondaryDnsServer -ne $IPAddressSecondaryServer){$lstSecondaryDnsServer += $DNSZoneSecondaryDnsServer}
+        } 
     
-    Set-DnsServerPrimaryZone -Name $DNSZone.ZoneName -Notify Notifyservers -notifyservers $lstSecondaryDnsServer -SecondaryServers $lstSecondaryDnsServer -SecureSecondaries TransferToSecureServers
+    Set-DnsServerPrimaryZone -ComputerName $DNSPrimaryServer -Name $DNSZone.ZoneName -Notify Notifyservers -notifyservers $lstSecondaryDnsServer -SecondaryServers $lstSecondaryDnsServer -SecureSecondaries TransferToSecureServers
+    }
 
-    return $(Get-DnsServerZone -Name $DNSZone.ZoneName | Select-Object ZoneName,SecondaryServers,NotifyServers)
+    return $(Get-DnsServerZone -ComputerName $DNSPrimaryServer -Name $DNSZone.ZoneName | Select-Object ZoneName,SecondaryServers,NotifyServers)
 }
 
 Remove-zDnsServerSecondaryServerTransferToSecureServers -ZoneName "teddycorp.lan" -IPAddressSecondaryServer "192.168.1.20"
